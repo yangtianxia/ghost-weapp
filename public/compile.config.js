706199ec225a1fb8043dd00080d823a7c1dda0e2
yangtianxia <1304082170@qq.com>
2024-07-31 22:04:30 +0800

    Feat add public compile.config.js

---
+++ public/compile.config.js
@@ -0,0 +1,146 @@
+module.exports = [
+  {
+    title: '首页',
+    page: 'pages/index/index',
+  },
+  {
+    title: '错误页',
+    page: 'subpackage/error/index',
+  },
+  {
+    title: 'button-示例',
+    page: 'examples/button/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'cascader-示例',
+    page: 'examples/cascader/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'grid-示例',
+    page: 'examples/grid/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'cell-示例',
+    page: 'examples/cell/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'checkbox-示例',
+    page: 'examples/checkbox/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'radio-示例',
+    page: 'examples/radio/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'result-示例',
+    page: 'examples/result/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'form-示例',
+    page: 'examples/form/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'alert-示例',
+    page: 'examples/alert/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'list-示例',
+    page: 'examples/list/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'float-panel-示例',
+    page: 'examples/float-panel/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'action-sheet-示例',
+    page: 'examples/action-sheet/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'count-down-示例',
+    page: 'examples/count-down/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'range-date-picker-示例',
+    page: 'examples/range-date-picker/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'popup-示例',
+    page: 'examples/popup/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'tabs-示例',
+    page: 'examples/tabs/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  },
+  {
+    title: 'toast-示例',
+    page: 'examples/toast/index',
+    query: '',
+    launchMode: 'default',
+    scene: null,
+    mode: ['development']
+  }
+]
